d0d27c0d65fa084bde3eca2981f2946bb6f729a8
anastasiyaKimstac <anastasiyakimstach@gmail.com>
2026-05-01 17:12:22 +0300

    Add contracts

---
+++ OOTP Lab3/Contracts/IPlugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OOTP_Lab3.Contracts
+{
+    /// <summary>
+    /// Interface for all plugins to implement
+    /// </summary>
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Unique identifier for the plugin
+        /// </summary>
+        string PluginId { get; }
+
+        /// <summary>
+        /// Plugin name displayed in UI
+        /// </summary>
+        string PluginName { get; }
+
+        /// <summary>
+        /// Plugin version
+        /// </summary>
+        string Version { get; }
+
+        /// <summary>
+        /// Initialize the plugin with host reference
+        /// </summary>
+        void Initialize(IPluginHost host);
+
+        /// <summary>
+        /// Get UI elements to add to main interface
+        /// </summary>
+        UIElement GetUIElement();
+
+        /// <summary>
+        /// Called when plugin is being unloaded
+        /// </summary>
+        void Shutdown();
+    }
+}
---
+++ OOTP Lab3/Contracts/IPluginHost.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using OOTP_Lab3.Models;
+
+namespace OOTP_Lab3.Contracts
+{
+    /// <summary>
+    /// Host interface that plugins can interact with
+    /// </summary>
+    public interface IPluginHost
+    {
+        /// <summary>
+        /// Add a new employee to the collection
+        /// </summary>
+        void AddEmployee(IEmployee employee);
+
+        /// <summary>
+        /// Remove an employee from the collection
+        /// </summary>
+        void RemoveEmployee(IEmployee employee);
+
+        /// <summary>
+        /// Get all employees
+        /// </summary>
+        ObservableCollection<IEmployee> GetEmployees();
+
+        /// <summary>
+        /// Show a message to the user
+        /// </summary>
+        void ShowMessage(string message, string title);
+
+        /// <summary>
+        /// Select an employee in the UI
+        /// </summary>
+        void SelectEmployee(IEmployee employee);
+
+        /// <summary>
+        /// Serialize all employees to file
+        /// </summary>
+        void SerializeToFile(string path);
+
+        /// <summary>
+        /// Deserialize employees from file
+        /// </summary>
+        void DeserializeFromFile(string path);
+    }
+}
